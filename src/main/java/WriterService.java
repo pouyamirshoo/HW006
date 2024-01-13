e7e998960744e68b92d6a81e7714d49ca2f90836
pc <poyatajhiz59@gmail.com>
2024-01-13 13:41:05 -0800

    add register method to WriterService

--- src/main/java/WriterService.java
+++ src/main/java/WriterService.java
@@ -1,2 +1,25 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class WriterService {
+
+    private final Scanner sc = new Scanner(System.in);
+    private final WriterRepository writerRepository = new WriterRepository();
+
+    public WriterService() throws SQLException {
+    }
+    public void register() throws SQLException {
+        System.out.println("Please enter your firstname:");
+        String firstname = sc.nextLine();
+
+        System.out.println("Please enter your lastname:");
+        String lastname = sc.nextLine();
+
+        System.out.println("Please enter your age:");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        Writer writer = new Writer(firstname, lastname, age);
+        writerRepository.save(writer);
+    }
 }
