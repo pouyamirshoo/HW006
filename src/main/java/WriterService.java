18f825a76fb2d2c3529cf1913d6ac54ec8f9ae8b
pc <poyatajhiz59@gmail.com>
2024-01-13 14:06:28 -0800

    add show writer to WriterService

--- src/main/java/WriterService.java
+++ src/main/java/WriterService.java
@@ -22,4 +22,13 @@ public class WriterService {
         Writer writer = new Writer(firstname, lastname, age);
         writerRepository.save(writer);
     }
+    public void showWriter() throws SQLException {
+        System.out.println("plz enter writer id");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        Writer writer = writerRepository.loud(id);
+        System.out.println(writer.getFirstname() + "," + writer.getLastname() + "," + writer.getAge());
+
+    }
 }
