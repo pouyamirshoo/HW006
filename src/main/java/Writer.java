5ab906ea7062499381690fc069a0e292eb7fefe5
pc <poyatajhiz59@gmail.com>
2024-01-13 12:32:54 -0800

    add fields and methods and constructions to writer class

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,2 +1,74 @@
+import java.util.Arrays;
+
 public class Writer {
+    import java.util.Arrays;
+
+    public class Writer {
+        private int writerId;
+        private String firstname;
+        private String lastname;
+        private int age;
+        Books[] books;
+
+        public Writer() {
+        }
+
+        public Writer(String firstname, String lastname, int age, Books[] books) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+            this.books = books;
+        }
+
+        public int getWriterId() {
+            return writerId;
+        }
+
+        public void setWriterId(int writerId) {
+            this.writerId = writerId;
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public Books[] getBooks() {
+            return books;
+        }
+
+        public void setBooks(Books[] books) {
+            this.books = books;
+        }
+
+        @Override
+        public String toString() {
+            return "Writer{" +
+                    "writerId=" + writerId +
+                    ", firstname='" + firstname + '\'' +
+                    ", lastname='" + lastname + '\'' +
+                    ", age=" + age +
+                    ", books=" + Arrays.toString(books) +
+                    '}';
+        }
+    }
 }
