1589a00315c8d500b41874f67e8ec75e9a769f6f
pc <poyatajhiz59@gmail.com>
2024-01-13 12:40:34 -0800

    establish connection to database in class

--- src/main/java/Jdbcconnection.java
+++ src/main/java/Jdbcconnection.java
@@ -1,2 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Jdbcconnection {
+    private final Connection connection =
+            DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
+
+    public Jdbcconnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
