50aeac5705fefbdbf73f338743c81a9e6e15ce9f
pc <poyatajhiz59@gmail.com>
2024-01-13 13:31:14 -0800

    add save method to writer class

--- src/main/java/WriterRepository.java
+++ src/main/java/WriterRepository.java
@@ -1,3 +1,5 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class WriterRepository {
@@ -5,5 +7,14 @@ public class WriterRepository {
 
     public WriterRepository() throws SQLException {
     }
+    public void save(Writer writer) throws SQLException {
+        Connection connection = jdbcConnection.getConnection();
 
+        String addWriter = "INSERT INTO writers(firstname,lastname,age) VALUES(?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(addWriter);
+
+        preparedStatement.setString(1, writer.getFirstname());
+        preparedStatement.setString(2, writer.getLastname());
+        preparedStatement.setInt(3, writer.getAge());
+    }
 }
