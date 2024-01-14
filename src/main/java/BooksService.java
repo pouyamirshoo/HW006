79fa7ebe9d8e03283acf11f928d84339ba8abf4b
pc <poyatajhiz59@gmail.com>
2024-01-13 20:29:40 -0800

    add addBook method to BooksService

--- src/main/java/BooksService.java
+++ src/main/java/BooksService.java
@@ -1,3 +1,24 @@
-public class BooksService {
+import java.sql.SQLException;
+import java.util.Scanner;
 
+public class BooksService {
+    private final Scanner sc = new Scanner(System.in);
+    private final BookRepository bookRepository = new BookRepository();
+
+    public BooksService() throws SQLException {
+    }
+    public void addBook() throws SQLException {
+        System.out.println("Please enter your book name:");
+        String bookName = sc.nextLine();
+
+        System.out.println("Please enter your date of publish as MM/dd/yyyy:");
+        String yearOfPublish = sc.nextLine();
+
+        System.out.println("Please enter the writer id:");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        Books book = new Books(bookName,yearOfPublish,id);
+        bookRepository.save(book);
+    }
 }
